b1391445137ea8b1f3afab51b9555e8921c096fe
Michael Ferguson <mike.ferguson2678@icloud.com>
2025-02-26 17:16:13 -0500

    Project
    App game done

--- odin-sketch/main.js
+++ odin-sketch/main.js
@@ -1,7 +1,21 @@
 let color = "black";
-
+let click = false;
 document.addEventListener("DOMContentLoaded", function () {
     createBoard(16);
+
+    document.querySelector("body").addEventListener("click", function (e) {
+      if (e.target.tagName != "BUTTON") {
+          click = !click;
+          let draw = document.querySelector("#draw");
+          if (click) {
+              draw.innerHTML = "Draw";
+          }
+          else {
+              draw.innerHTML = "Stop Drawing";
+          }
+      }
+        
+    })
     
     let btn_popup = document.querySelector("#popup");
     btn_popup.addEventListener("click", function () {
@@ -51,15 +65,16 @@ function getSize() {
 
 }
 function colorDiv() {
-    if (color == "random") {
-        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
-    }
+    if (click) {
+        if (color == "random") {
+            this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+        }
 
-    else {
-        this.style.backgroundColor = 'black';
+        else {
+            this.style.backgroundColor = 'black';
+        }
     }
 }
-
     function setColor(colorChoice) {
          color = colorChoice;
     
